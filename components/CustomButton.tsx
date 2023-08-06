f4f8be22e933bb5cff006f8c47080aeb74b25b78
Aryan Garg <gargaryan82000@gmail.com>
2023-08-06 11:34:10 +0530

    Added All The Project Files

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 
-import { CustomButtonProps } from "@types";
+import { CustomButtonProps } from "@/types";
 
 const Button = ({ isDisabled, btnType, containerStyles, textStyles, title, rightIcon, handleClick }: CustomButtonProps) => (
     <button
